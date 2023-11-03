ff8b233ac6aa4465e51e61979c98d9a5fbbef947
mara karma <marakarma36@gmail.com>
2023-11-03 13:15:00 +0700

    initial commit 2

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,58 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { AdminfooterComponent } from './partials/adminfooter/adminfooter.component';
+import { AdminheaderComponent } from './partials/adminheader/adminheader.component';
+import { ActivityComponent } from './activity/activity.component';
+import { ChannelComponent } from './channel/channel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SocialComponent } from './social/social.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children:[
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'user',
+        component: UserComponent,
+      },
+      {
+        path: 'activity',
+        component: ActivityComponent,
+      },
+      {
+        path: 'social',
+        component: SocialComponent,
+      },
+      {
+        path: 'channel',
+        component: ChannelComponent,
+      }
+    ]
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    AdminfooterComponent,
+    AdminheaderComponent,
+    ActivityComponent,
+    ChannelComponent,
+    DashboardComponent,
+    SocialComponent,
+    UserComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AdminModule { }
--- src/app/guest/guest.module.ts
+++ src/app/guest/guest.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GuestComponent } from './guest.component';
+import { AboutComponent } from './about/about.component';
+import { BusinessComponent } from './business/business.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { SuggestionComponent } from './suggestion/suggestion.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GuestComponent,
+    AboutComponent,
+    BusinessComponent,
+    ContactComponent,
+    HelpComponent,
+    HomeComponent,
+    SuggestionComponent,
+    PricingComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
